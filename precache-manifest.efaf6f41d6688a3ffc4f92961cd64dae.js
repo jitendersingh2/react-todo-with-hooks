b82b09cbb3c173481997fb8bf30b65c58520fd71
Jitender Singh <C-Jitender.Singh@charter.com>
2020-08-09 19:23:00 +0530

    Updates

---
+++ precache-manifest.efaf6f41d6688a3ffc4f92961cd64dae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f2172b8afb775619d04a5806b5969ec",
+    "url": "/react-todo-with-hooks/index.html"
+  },
+  {
+    "revision": "4434f0dc54bdf424bd67",
+    "url": "/react-todo-with-hooks/static/css/main.e579999d.chunk.css"
+  },
+  {
+    "revision": "bcf61912bf17af7f6214",
+    "url": "/react-todo-with-hooks/static/js/2.43ba1409.chunk.js"
+  },
+  {
+    "revision": "85b12af60ddf57ea6860c3b16407fd52",
+    "url": "/react-todo-with-hooks/static/js/2.43ba1409.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4434f0dc54bdf424bd67",
+    "url": "/react-todo-with-hooks/static/js/main.8cf50388.chunk.js"
+  },
+  {
+    "revision": "90fff4f285804ba12644",
+    "url": "/react-todo-with-hooks/static/js/runtime-main.56a319ab.js"
+  }
+]);
